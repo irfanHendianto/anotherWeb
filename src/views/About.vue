a131961bc16175ad0acffe8163bcacdcc52b7c2b
irfan hendianto wijaya <irfanhendianto@gmail.com>
2020-06-16 14:56:24 +0700

    revisi

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,42 +3,120 @@
     <v-subheader>About</v-subheader>
     
     <v-container>
-      <v-card class="pa-4">
-      <p>Contrary to popular belief, Lorem Ipsum is not simply random text. 
-        It has roots in a piece of classical Latin literature from 45 BC, 
-        making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-         looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word 
-         in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
-         "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
-          This book is a treatise on the theory of ethics, very popular during the Renaissance.
-         The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32
-         Contrary to popular belief, Lorem Ipsum is not simply random text. 
-        It has roots in a piece of classical Latin literature from 45 BC, 
-        making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-         looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word 
-         in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
-         "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
-          This book is a treatise on the theory of ethics, very popular during the Renaissance.
-         The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32
-         Contrary to popular belief, Lorem Ipsum is not simply random text. 
-        It has roots in a piece of classical Latin literature from 45 BC, 
-        making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-         looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word 
-         in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
-         "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
-          This book is a treatise on the theory of ethics, very popular during the Renaissance.
-         The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32
-         Contrary to popular belief, Lorem Ipsum is not simply random text. 
-        It has roots in a piece of classical Latin literature from 45 BC, 
-        making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-         looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word 
-         in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
-         "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.
-          This book is a treatise on the theory of ethics, very popular during the Renaissance.
-         The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32
-         asdas
-         </p>
-      </v-card>
+      <v-layout row wrap>
+        <v-flex sm4 xs12>
+           <v-card class="mr-10 ma-5">
+             <v-card-text>
+               <div>Word of the Day</div>
+                  <p class="display-1 text--primary">
+                    be•nev•o•lent
+                  </p>
+                  <p>adjective</p>
+                  <div class="text--primary">
+                    well meaning and kindly.<br>
+                    "a benevolent smile"
+                </div>
+             </v-card-text>
+            <v-card-actions>
+              <v-btn
+                text
+                color="deep-purple accent-4"
+              >
+                Learn More
+              </v-btn>
+            </v-card-actions>
+           </v-card>
+        </v-flex>
+
+        <v-flex sm4 xs12 >
+          <v-card
+            class="mr-10 ma-5"
+            max-width="400"
+          >
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            >
+              <v-card-title>Top 10 Australian beaches</v-card-title>
+            </v-img>
+
+            <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+            <v-card-text class="text--primary">
+              <div>Whitehaven Beach</div>
+
+              <div>Whitsunday Island, Whitsunday Islands</div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+              >
+                Share
+              </v-btn>
+
+              <v-btn
+                color="orange"
+                text
+              >
+                Explore
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+
+        <v-flex sm4 xs12 >
+          <v-card
+            class="mr-12 ma-5"
+            max-width="344"
+          >
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            ></v-img>
+
+            <v-card-title>
+              Top western road trips
+            </v-card-title>
+
+            <v-card-subtitle>
+              1,000 miles of wonder
+            </v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text>Share</v-btn>
+
+              <v-btn
+                color="purple"
+                text
+              >
+                Explore
+              </v-btn>
+
+              <v-spacer></v-spacer>
+
+              <v-btn
+                icon
+                @click="show = !show"
+              >
+                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+
+            <v-expand-transition>
+              <div v-show="show">
+                <v-divider></v-divider>
+
+                <v-card-text>
+                  I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+                </v-card-text>
+              </div>
+            </v-expand-transition>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
